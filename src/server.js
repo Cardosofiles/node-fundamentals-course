c85bd272c16c9dbc6d2e0023096b1e76a03e977a
cardosofiles <cardosofiles@outlook.com>
2024-11-14 13:57:07 -0300

    example of servers with node.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,11 @@
 import http from "node:http";
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  const { method, url } = req;
+
+  console.log(method, url);
+  return res.end("hello Javascript and TypeScript");
+});
 
 server.listen(3333, () => {
   console.log("Server is running on port 3333");
