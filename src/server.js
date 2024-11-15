b84de0a217efc5ef7af7c7453d9a12bab2776ad4
cardosofiles <cardosofiles@outlook.com>
2024-11-15 15:09:44 -0300

    creating routes to exemplify requests in the API

--- src/server.js
+++ src/server.js
@@ -1,9 +1,38 @@
 import http from "node:http";
 
+/**
+ * - Crair usuários
+ * - Listagem de usuários
+ * - Edição de Usuários
+ * - Remoção de Usuários
+ *
+ * - HTTP
+ *  - Método
+ *  - URL
+ *
+ * GET, POST, PUT, PATCH, DELETE
+ *
+ * GET => Bucar um recurso do back-end
+ * POST => Criar um recurso no back-end
+ * PUT => Atualizar um recurso no back-en
+ * PATCH => Atualizar um recurso específico no back-end
+ * DELETE => Deletar um recurso no back-end
+ *
+ * GET / users => Buscando usuários do back-end
+ * POST / users => Criar um usuário no back-end
+ */
+
 const server = http.createServer((req, res) => {
   const { method, url } = req;
 
-  console.log(method, url);
+  if (method === "GET" && url === "/users") {
+    return res.end("Listando os usuários");
+  }
+
+  if (method === "POST" && url === "/users") {
+    return res.end("Criando um novo usuário");
+  }
+
   return res.end("hello Javascript and TypeScript");
 });
 
