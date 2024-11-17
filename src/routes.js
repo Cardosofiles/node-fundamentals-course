6d306e8652ecb98c2ea40d5c0e0c5b7f40e275fe
cardosofiles <cardosofiles@outlook.com>
2024-11-16 22:48:08 -0300

    separating the API routes into a new file

---
+++ src/routes.js
@@ -0,0 +1,45 @@
+import { randomUUID } from "node:crypto";
+import { Database } from "./database.js";
+
+const database = new Database();
+
+export const routes = [
+  {
+    method: "GET",
+    path: "/users",
+    handler: (req, res) => {
+      const users = database.select("users");
+
+      return res
+        .setHeader("Content-type", "application/json")
+        .end(JSON.stringify(users));
+    },
+  },
+  {
+    method: "POST",
+    path: "/users",
+    handler: (req, res) => {
+      if (!req.body || !req.body.name || !req.body.email) {
+        return res.writeHead(400).end("Bad Request: Missing name or email");
+      }
+
+      const { name, email } = req.body;
+      const users = database.select("users") || [];
+
+      const user = {
+        /**
+         * Duas opção para gerar o ID
+         * "id: users.length + 1", cria um sequencial
+         * "id: randomUUID()", gera um id aleatório para o registro
+         */
+        id: randomUUID(),
+        name,
+        email,
+      };
+
+      database.insert("users", user);
+
+      return res.writeHead(201).end("User created successfully");
+    },
+  },
+];
--- src/server.js
+++ src/server.js
@@ -1,46 +1,19 @@
 import http from "node:http";
 
-import { randomUUID } from "node:crypto";
-import { Database } from "./database.js";
 import { json } from "./middlewares/json.js";
-
-const database = new Database();
+import { routes } from "./routes.js";
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
   await json(req, res);
 
-  if (method === "GET" && url === "/users") {
-    const users = database.select("users");
+  const route = routes.find((r) => {
+    return r.method === method && r.path === url;
+  });
 
-    return res
-      .setHeader("Content-type", "application/json")
-      .end(JSON.stringify(users));
-  }
-
-  if (method === "POST" && url === "/users") {
-    if (!req.body || !req.body.name || !req.body.email) {
-      return res.writeHead(400).end("Bad Request: Missing name or email");
-    }
-
-    const { name, email } = req.body;
-    const users = database.select("users") || [];
-
-    const user = {
-      /**
-       * Duas opção para gerar o ID
-       * "id: users.length + 1", cria um sequencial
-       * "id: randomUUID()", gera um id aleatório para o registro
-       */
-      id: randomUUID(),
-      name,
-      email,
-    };
-
-    database.insert("users", user);
-
-    return res.writeHead(201).end("User created successfully");
+  if (route) {
+    return route.handler(req, res);
   }
 
   return res.writeHead(404).end("Not Found");
